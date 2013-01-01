9f23db3eff4f9e96cd96274f1a2cda395905d1a4
elcanadiano <scribbleseq@gmail.com>
2012-12-31 19:46:17 -0800

    "Modified copyright message"

--- donate/postcaptcha.php
+++ donate/postcaptcha.php
@@ -1,10 +1,25 @@
 <?php
 /* ******************** Penguin Post ********************
  *
- * By Alexander Poon
  * Website: http://tc-pub.com
- * License: 
+ *
+ * Copyright (C)2012 Alexander Poon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
+ * ******************************************************
+ *
  * postcaptcha.php handles the CAPTCHA portion of the
  * script. When called, it will spit out a question to
  * penguinpost.js or verify if a question was answered
